167b7ee27104c28a7b4815ae7f2aafdfa48a04d9
DFMoller <58703749+DFMoller@users.noreply.github.com>
2020-04-05 23:41:04 +0200

    Worked on html, css and js. Used gimp to improve images.

--- my_app.js
+++ my_app.js
@@ -1,1 +1,7 @@
-console.log('ready');
+$(document).ready(function(){
+	$('.cover').animate({height: '100%'}, 1000, 'swing')
+		.animate({width: '80%'}, 1200, 'swing')
+	$('.slider').delay(1000).animate({left: '100%'}, 1200, 'swing')
+	// $('nav').delay(1000).animate({left: '100%'}, 1200, 'swing')
+	// $('nav').transition({x: 0}, 1200, 'swig')
+});
