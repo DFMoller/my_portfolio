24a44ae05469a7d9e7f003be3e23372174262195
DFMoller <58703749+DFMoller@users.noreply.github.com>
2020-04-07 17:55:18 +0200

    Incuded section 3 animation

--- my_app.js
+++ my_app.js
@@ -6,9 +6,35 @@ $(document).ready(function(){
 	// Init Scroll Magic
 	var controller = new ScrollMagic.Controller();
 
-	var ourScene = new ScrollMagic.Scene({
-		triggerElement: '#section2'
+	//  Build a scene--SUN-LOGO
+	var sunTopHalfScene = new ScrollMagic.Scene({
+		triggerElement: '#section2',
+		triggerHook: 0.6
+		// This is where triggerHook will go: triggerHook: 1 for example. Remember to add comma after last command...
+		// reverse: false --- if we want the animation to only happen once...
 	})
 	.setClassToggle('#sun-top-half', 'fade-in') //add class to sun-top-half
+	// .addIndicators({})
 	.addTo(controller);
+
+	// SUN-SLIDER
+	var sunSlider = new ScrollMagic.Scene({
+		triggerElement: '#section2',
+		// reverse: false
+		triggerHook: 0.6
+	})
+	.setClassToggle('#slider2', 'slide-down') //add class to sun-top-half
+	.addTo(controller);
+
+
+
+	var slider3 = new ScrollMagic.Scene({
+		triggerElement: '#section3',
+		// reverse: false
+		triggerHook: 0.25
+	})
+	.setClassToggle('#slider3', 'slide-from-right') //add class to sun-top-half
+	// .addIndicators({})
+	.addTo(controller);
+
 });
