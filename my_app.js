64109567785fa8815f64b29553d47bbed59fdfc6
DFMoller <58703749+DFMoller@users.noreply.github.com>
2020-04-07 09:19:27 +0200

    Update my_app.js
    Added the SUN animation.

--- my_app.js
+++ my_app.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 	$('.cover').animate({height: '100%'}, 1000, 'swing')
 		.animate({width: '80%'}, 1200, 'swing')
-	$('.slider').delay(1000).animate({left: '100%'}, 1200, 'swing')
-	// $('nav').delay(1000).animate({left: '100%'}, 1200, 'swing')
-	// $('nav').transition({x: 0}, 1200, 'swig')
+	$('#slider1').delay(1000).animate({left: '100%'}, 1200, 'swing')
+
+	// Init Scroll Magic
+	var controller = new ScrollMagic.Controller();
+
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '#section2'
+	})
+	.setClassToggle('#sun-top-half', 'fade-in') //add class to sun-top-half
+	.addTo(controller);
 });
