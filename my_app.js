10c33333a05a129ee323699ec228782d2eaa54e4
DFMoller <58703749+DFMoller@users.noreply.github.com>
2020-04-13 14:14:44 +0200

    Update my_app.js
    Animater the disclaimer

--- my_app.js
+++ my_app.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 		$('.cover').animate({height: '100%'}, 1000, 'swing')
 			.animate({width: '80%'}, 1200, 'swing')
 		$('#slider1').delay(1000).animate({left: '100%'}, 1200, 'swing')
+		$('#disclaimer').delay(2000).animate({top: '45%'}, 1000, 'swing')
 	})
 	
 
